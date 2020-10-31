4400e78184c01eeffbaca29c6b631aa97cfbfb68
Justin Nguyen <8205858+nguyenhjustin@users.noreply.github.com>
2020-10-31 23:25:46 +0800

    Create Home class which extends StatelessWidget to allow hot reload

--- lib/main.dart
+++ lib/main.dart
@@ -2,38 +2,45 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'This is a title!',
-            style: TextStyle(
-              color: Colors.blue[900],
-              fontWeight: FontWeight.bold,
-            ),
+      home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'This is a title!',
+          style: TextStyle(
+            color: Colors.blue[900],
+            fontWeight: FontWeight.bold,
           ),
-          centerTitle: true,
-          backgroundColor: Colors.greenAccent,
         ),
-        body: Center(
-          child: Text(
-            'Hello World!',
-            style: TextStyle(
+        centerTitle: true,
+        backgroundColor: Colors.greenAccent,
+      ),
+      body: Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(
               color: Colors.blueAccent,
               fontSize: 30.0,
               fontWeight: FontWeight.bold
-            ),
           ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Text(
-            'Click Me!',
-            style: TextStyle(
-                color: Colors.blue[900],
-            ),
-          ),
-          backgroundColor: Colors.greenAccent,
         ),
       ),
-  ));
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text(
+          'Click Me!',
+          style: TextStyle(
+            color: Colors.blue[900],
+          ),
+        ),
+        backgroundColor: Colors.greenAccent,
+      ),
+    );
+  }
 }
